429d6503321432afacedfab3035e651879cc19e7
Denise Fernandez <dcbfernandez@gmail.com>
2014-02-13 23:29:11 +1100

    Tidying up the code

--- src/main/java/dynamic_proxies/caching/CacheProxy.java
+++ src/main/java/dynamic_proxies/caching/CacheProxy.java
@@ -23,18 +23,24 @@ public class CacheProxy implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object result;
         try {
-            //Not sure if this is ok to mash casting logic into what is essentially an implementation agnostic class?
-            Purchase purchase = (Purchase) args[0];
-
-            //This is a sysout of what we might write to redis. It means we don't need to tie this caching logic into our main class logic.
-            System.out.println("Someone from "+purchase.getCustomerSuburb() + " just bought a "+purchase.getItemDescription()+ "!");
+            if (args.length > 0) {
+                if (args[0] instanceof Purchase) {
+                    Purchase purchase = (Purchase) args[0];
+                    //This is a sysout of what we might write to a redis store. It means we don't need to tie this caching logic into our main class logic.
+                    System.out.println("Someone from "+purchase.getCustomerSuburb() + " just bought a "+purchase.getItemDescription()+ "!");
+                } else {
+                    System.out.println("Proxy for a method which does not have a purchase as the first argument: " + args[0]);
+                }
+            } else {
+                System.out.println("Proxy for a method that is being passed with no arguments.");
+            }
             result = method.invoke(realObject, args);
         } catch (InvocationTargetException e) {
             throw e.getTargetException();
         } catch (Exception e) {
-            throw new RuntimeException("unexpected invocation exception: " + e.getMessage());
+            throw new RuntimeException("Unexpected invocation exception: " + e.getMessage(), e);
         } finally {
-            System.out.println("after method " + method.getName());
+            System.out.println("After method invocation with method name: " + method.getName());
         }
         return result;
     }
